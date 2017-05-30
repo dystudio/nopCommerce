1b039de96ffc8fac73e229abeeacff8abe6eda82
david <david.bingjian.yu>
2017-05-30 11:14:31 +0800

    added

---
+++ src/Plugins/Nop.Plugin.Misc.WeChat/RouteProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Nop.Web.Framework.Mvc.Routes;
+
+namespace Nop.Plugin.Misc.WeChat
+{
+    public class RouteProvider : IRouteProvider
+    {
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute("Nop.Plugin.Misc.WeChat.Index",
+                 "WeChat/",
+                 new { controller = "WeChat", action = "Index", },
+                 new[] { "Nop.Plugin.Misc.WeChat.Controllers" }
+            );
+        }
+
+        public int Priority
+        {
+            get { return 0; }
+        }
+    }
+}
