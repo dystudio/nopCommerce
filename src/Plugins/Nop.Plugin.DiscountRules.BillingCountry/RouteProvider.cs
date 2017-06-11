5af9c599a813b97157fa9eff9666e409c8b54530
david <david.bingjian.yu>
2017-06-11 18:59:47 +0800

    billing and shipping discount rule plugin aded

---
+++ src/Plugins/Nop.Plugin.DiscountRules.BillingCountry/RouteProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Nop.Web.Framework.Mvc.Routes;
+
+namespace Nop.Plugin.DiscountRules.BillingCountry
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        #region Methods
+
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute("Plugin.DiscountRules.BillingCountry.Configure",
+                 "Plugins/DiscountRulesBillingCountry/Configure",
+                 new { controller = "DiscountRulesBillingCountry", action = "Configure" },
+                 new[] { "Nop.Plugin.DiscountRules.BillingCountry.Controllers" }
+            );
+        }
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        #endregion
+    }
+}
