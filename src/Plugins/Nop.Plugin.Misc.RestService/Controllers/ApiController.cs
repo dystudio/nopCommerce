5b2b088798e39c1be92046f6bc5e7dd5ccddb21e
david <david.bingjian.yu>
2017-06-26 13:28:27 +0800

    update

--- src/Plugins/Nop.Plugin.Misc.RestService/Controllers/ApiController.cs
+++ src/Plugins/Nop.Plugin.Misc.RestService/Controllers/ApiController.cs
@@ -88,10 +88,9 @@ namespace Nop.Plugin.Misc.RestService.Controllers
             var sslEnabled = _settings.SslEnabled;
 
 
-            var scheme = sslEnabled ? "https" : "http";      
-            var serverUrl = Request.Url.IsDefaultPort ? 
-                string.Format(@"{0}://{1}", scheme, Request.Url.Host) : string.Format(@"{0}://{1}:{2}", scheme, Request.Url.Host,Request.Url.Port);
-
+            var scheme = sslEnabled ? "https" : "http";
+            var serverUrl = string.Format(@"{0}://{1}", scheme, Request.Url.Host);
+ 
             var redirectUrl = string.Format(@"{0}/RestApi/GetToken",serverUrl);
             var client = new RestClient(serverUrl);
             var request = new RestRequest("/oauth/authorize", Method.GET);
